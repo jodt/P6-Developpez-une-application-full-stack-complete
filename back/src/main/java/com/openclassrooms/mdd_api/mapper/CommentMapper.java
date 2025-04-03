d682056d8b83cb044a60e9a5f893af21e3040f64
joel Dumortier <joel.dumortier@gmail.com>
2025-04-03 22:01:56 +0200

    update mapper to remove warnings

--- back/src/main/java/com/openclassrooms/mdd_api/mapper/CommentMapper.java
+++ back/src/main/java/com/openclassrooms/mdd_api/mapper/CommentMapper.java
@@ -2,23 +2,29 @@ package com.openclassrooms.mdd_api.mapper;
 
 import com.openclassrooms.mdd_api.dto.CommentDto;
 import com.openclassrooms.mdd_api.model.Comment;
-import com.openclassrooms.mdd_api.model.User;
 import com.openclassrooms.mdd_api.service.PostService;
-import com.openclassrooms.mdd_api.service.UserService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
+import org.mapstruct.Mappings;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring", imports = {PostService.class})
 public interface CommentMapper {
 
-    @Mapping(target = "post", expression = "java(postService.findPostById(commentDto.getPostId()).orElse(null))")
-    @Mapping(target = "author", ignore = true )
+    @Mappings({
+            @Mapping(target = "post", expression = "java(postService.findPostById(commentDto.getPostId()).orElse(null))"),
+            @Mapping(target = "author", ignore = true),
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "createdAt", ignore = true)
+    })
     Comment asComment(CommentDto commentDto, PostService postService);
 
-    @Mapping(target = "author", expression = "java(comment.getAuthor() != null ? comment.getAuthor().getUserName() : null)")
+    @Mappings({
+            @Mapping(target = "author", expression = "java(comment.getAuthor() != null ? comment.getAuthor().getUserName() : null)"),
+            @Mapping(target = "postId", ignore = true )
+    })
+
     CommentDto asCommentDto(Comment comment);
     List<CommentDto> asCommentDtos (List<Comment> comments);
 
