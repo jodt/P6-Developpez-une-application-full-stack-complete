4c850537abe81a946846f29b882b3a2eaecf5fed
joel Dumortier <joel.dumortier@gmail.com>
2025-04-03 17:28:25 +0200

    add the topic in the dto

--- back/src/main/java/com/openclassrooms/mdd_api/mapper/PostMapper.java
+++ back/src/main/java/com/openclassrooms/mdd_api/mapper/PostMapper.java
@@ -4,13 +4,18 @@ import com.openclassrooms.mdd_api.dto.PostDto;
 import com.openclassrooms.mdd_api.model.Post;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface PostMapper {
 
-    @Mapping(target = "author", expression = "java(post.getAuthor().getUserName())")
+    @Mappings({
+            @Mapping(target = "author", expression = "java(post.getAuthor().getUserName())"),
+            @Mapping(target = "topic", expression = "java(post.getTopic().getTitle())")
+    })
+
     PostDto asPostDto(Post post);
 
     List<PostDto> asPostDtos (List<Post> posts);
