93ab6faf12621b9872753de67d355f6b9da71169
joel Dumortier <joel.dumortier@gmail.com>
2025-04-04 16:29:22 +0200

    implement login page

--- front/src/app/features/auth/auth.module.ts
+++ front/src/app/features/auth/auth.module.ts
@@ -7,17 +7,20 @@ import {ReactiveFormsModule} from "@angular/forms";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
+import { LoginComponent } from './components/login/login.component';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
-  declarations: [RegisterComponent],
+  declarations: [RegisterComponent, LoginComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class AuthModule { }
