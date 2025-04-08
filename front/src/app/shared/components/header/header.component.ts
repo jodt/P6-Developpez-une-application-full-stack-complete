e6fdcd4a2cc681cb955a8a4102125108516325c6
joel Dumortier <joel.dumortier@gmail.com>
2025-04-08 19:03:00 +0200

    manage header display in header component instead of app component

--- front/src/app/shared/components/header/header.component.ts
+++ front/src/app/shared/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SessionService } from '../../../services/session.service';
-import { Observable, of } from 'rxjs';
+import { filter, Observable, of } from 'rxjs';
+import { NavigationEnd, Router } from '@angular/router';
+import { MobileService } from '../../../services/mobile.service';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +10,36 @@ import { Observable, of } from 'rxjs';
   styleUrl: './header.component.css',
 })
 export class HeaderComponent implements OnInit {
-  isLogged$: Observable<boolean> = of(false);
+  showToolbar: boolean = true;
+  isMobileResolution = false;
+  isLogged: boolean = false;
 
-  constructor(private sessionService: SessionService) {}
-  
+  constructor(private sessionService: SessionService, private router: Router, private mobileService: MobileService
+  ) {}
+
   ngOnInit(): void {
-    this.isLogged$ = this.sessionService.$isLogged();
+    this.isMobile();
+
+    this.sessionService.$isLogged().subscribe((response) => {
+      this.isLogged = response;
+      this.router.events.pipe(
+        filter(event => event instanceof NavigationEnd)).subscribe(() => {
+          this.displayToolBar();
+      });
+    });
+  }
+
+  logout() {
+    this.sessionService.logOut();
+    this.router.navigate(['/'])
+  }
+
+  private displayToolBar() {
+    this.showToolbar = (!this.isLogged && !this.isMobileResolution && (this.router.url.includes("register")) || this.router.url.includes("login")) || this.isLogged;
+    console.log((this.router.url.includes("register")) || this.router.url.includes("login"))
+  }
+
+  private isMobile() {
+    this.isMobileResolution = this.mobileService.isMobile();
   }
 }
